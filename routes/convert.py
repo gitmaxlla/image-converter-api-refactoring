2a31c8c7e2c9766bc04a36a97af167063d7af2fc
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-11-15 15:27:31 -0500

    delete: unnecessary arg

--- routes/convert.py
+++ routes/convert.py
@@ -1,11 +1,10 @@
 from PIL import Image
 
-from ..utils.constants import ALLOWED_EXTENSIONS
 from ..adapters.models.image import ImageConvertModel
 from ..usecases.convert_image import convert_image
 from ..adapters.controllers.image import ImageConvertController
 
-image_model = ImageConvertModel(Image, ALLOWED_EXTENSIONS)
+image_model = ImageConvertModel(Image)
 handle_image_convert = convert_image(image_model)
 
 image_controller = ImageConvertController(handle_image_convert)
